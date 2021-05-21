9ecea005bed562a7b9ff3290c12c37f45103021e
gangsta-cooldudeacool <84027949+gangsta-cooldudeacool@users.noreply.github.com>
2021-05-21 13:57:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var reasons= [
+    "depika srivastava(my mom)",
+    "Dheeraj kumar srivastava(my dad)",
+    "Avni Srvastava(my sister)"]
+  
+  var i = 0;
+  function nextslide(){document.getElementById("reeeee").
+                      innerHTML = reasons[i];
+     i++;
+                       
+                      }
+  
